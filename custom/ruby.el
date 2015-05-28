5bbadeeb56532b45ea6da147169c23dba78becc9
Thiago A. Silva <thiagoaraujos@gmail.com>
2015-05-28 00:10:51 -0300

    Simplify / evolve Ruby setup
    Get rid of comment clutter!

--- custom/ruby.el
+++ custom/ruby.el
@@ -1,79 +1,33 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; SETUP ENHANCED RUBY MODE ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;(add-to-list 'ac-modes 'enh-ruby-mode)
-(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
-
-(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 
-; Enable line numbers with meta hook
+(add-hook 'enh-ruby-mode-hook 'projectile-rails-on)
+(add-hook 'enh-ruby-mode-hook 'ruby-end-mode)
+(add-hook 'enh-ruby-mode-hook 'robe-mode)
 (add-hook 'enh-ruby-mode-hook
-	  (lambda () (run-hooks 'linum-mode-hook)))
-
-;;;;;;;;;;;
-;; IDENT ;;
-;;;;;;;;;;;
+	  (lambda () (rvm-activate-corresponding-ruby)))
+(add-hook 'enh-ruby-mode-hook
+	  (lambda () (ignore-errors(linum-mode 1))))
 
 (setq enh-ruby-bounce-deep-indent t)
 (setq enh-ruby-hanging-brace-indent-level 2)
 (setq ruby-deep-indent-paren nil)
 
-;;;;;;;;;
-;; RVM ;;
-;;;;;;;;;
-
-(require 'rvm)
-(rvm-use-default)
-(add-hook 'ruby-mode-hook (lambda () (rvm-activate-corresponding-ruby)))
-
-;;;;;;;;;;;
-;; RSPEC ;;
-;;;;;;;;;;;
-
-(require 'rspec-mode)
 (setq rspec-use-rake-when-possible nil)
-
-;; Use bash for RSpec (ZSH compatibility issues)
 (defadvice rspec-compile (around rspec-compile-around)
   (let ((shell-file-name "/bin/bash"))
     ad-do-it))
-
 (ad-activate 'rspec-compile)
 
-;;;;;;;;;;;;;
-;; PLUGINS ;;
-;;;;;;;;;;;;;
-
-(require 'ruby-tools)
-;(require 'rcodetools)
-(require 'rhtml-mode)
-(require 'haml-mode)
-
-(add-hook 'enh-ruby-mode-hook 'projectile-rails-on)
-(add-hook 'enh-ruby-mode-hook 'robe-mode)
+(require 'rcodetools)
 
 (defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
   (rvm-activate-corresponding-ruby))
 
-;; Disable auto insert of encoding comment (was useful for 1.9)
-(setq ruby-insert-encoding-magic-comment nil)
-
-;;;;;;;;;;;;;;
-;; MAPPINGS ;;
-;;;;;;;;;;;;;;
-
-;; Fire IRB session
 (global-set-key (kbd "C-c r r") 'inf-ruby)
-
-;; Live ruby results with xmpfilter (from rcodetools)
-(define-key ruby-mode-map (kbd "C-c C-c") 'xmp)
-
-;; Magic easy string interpolation
-(define-key ruby-mode-map (kbd "#") 'senny-ruby-interpolate)
+(global-set-key (kbd "C-c C-c") 'xmp)
