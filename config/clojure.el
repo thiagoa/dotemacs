2316134f1864a75ce50155eb6386eb035b3fa87b
Thiago A. Silva <thiagoaraujos@gmail.com>
2018-05-14 20:36:18 -0300

    Sweeter syntax

--- config/clojure.el
+++ config/clojure.el
@@ -1,4 +1,4 @@
 (add-hook 'clojure-mode-hook 'paredit-mode)
-(add-hook 'clojure-mode-hook #'linum-mode)
-(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
-(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook 'linum-mode)
+(add-hook 'clojure-mode-hook 'aggressive-indent-mode)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
--- config/emacslisp.el
+++ config/emacslisp.el
@@ -1,3 +1,3 @@
-(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
-(add-hook 'emacs-lisp-mode-hook #'linum-mode)
-(add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
+(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook 'linum-mode)
+(add-hook 'emacs-lisp-mode-hook 'aggressive-indent-mode)
