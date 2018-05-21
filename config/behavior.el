0da506c1452abf61f251d55cd5eca5323e7103d1
Thiago A. Silva <thiagoaraujos@gmail.com>
2018-05-21 00:05:39 -0300

    Simplify behavior.el, configure ido-vertical

--- config/behavior.el
+++ config/behavior.el
@@ -1,4 +1,5 @@
 (require 'helm-files)
+(require 'ido-vertical-mode)
 
 (emacs-use-same-path-as-shell)
 (config-terminal-encoding)
@@ -8,23 +9,36 @@
 (projectile-global-mode)
 (global-discover-mode 1)
 (global-undo-tree-mode 1)
-(winner-mode 1) ; Revert panes with C-c (left or right)
+(winner-mode 1)
+(ido-vertical-mode)
 
 (defvar newline-and-indent t)
-
-(setq projectile-enable-caching t) ;; "C-u C-c p f" to force reload.
+(setq projectile-enable-caching t)
 (setq backup-directory-alist `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
-(setq ns-right-alternate-modifier nil) ; Leave right option key for Mac
+(setq ns-right-alternate-modifier nil)
 (setq system-uses-terminfo nil)
 (setq eshell-where-to-jump 'begin)
 (setq eshell-review-quick-commands nil)
 (setq eshell-smart-space-goes-to-end t)
+(setq compilation-scroll-output 'first-error)
 (setq hippie-expand-try-functions-list
       '(try-expand-line
 	try-expand-line-all-buffers
 	try-complete-file-name
 	try-complete-lisp-symbol))
+(setq ido-enable-flex-matching t
+      ido-create-new-buffer 'always
+      ido-use-faces t)
+(setq dired-dwim-target t)
 
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
+
+(add-hook 'before-save-hook 'whitespace-cleanup)
+(add-hook 'ido-setup-hook
+	  '(lambda ()
+	     (define-key ido-completion-map "\C-h" 'ido-delete-backward-updir)
+	     (define-key ido-completion-map "\C-n" 'ido-next-match)
+	     (define-key ido-completion-map "\C-p" 'ido-prev-match)
+	     (define-key ido-completion-map " " 'ido-exit-minibuffer)))
