a138a58af0d5fb5d286572319122ac76b51e2947
Thiago A. Silva <thiagoaraujos@gmail.com>
2018-05-20 23:50:18 -0300

    Use dracula as default theme

--- config/looknfeel.el
+++ config/looknfeel.el
@@ -4,7 +4,7 @@
 (add-to-list 'default-frame-alist '(font . "-*-Menlo-medium-normal-normal-*-16-*-*-*-m-0-iso10646-1"))
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-(load-theme 'molokai t)
+(load-theme 'dracula t)
 
 (menu-bar-mode -1)
 (if (functionp 'tool-bar-mode) (tool-bar-mode -1))
