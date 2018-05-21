50e0d8dabcd84f18a886fa8de29d2fb810deebbf
Thiago A. Silva <thiagoaraujos@gmail.com>
2018-05-20 23:49:11 -0300

    Use rainbow delimiters mode in Emacs Lisp

--- config/emacslisp.el
+++ config/emacslisp.el
@@ -1,5 +1,6 @@
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
 (add-hook 'emacs-lisp-mode-hook 'linum-mode)
 (add-hook 'emacs-lisp-mode-hook 'aggressive-indent-mode)
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 
 (add-to-list 'auto-mode-alist '("Cask$" . emacs-lisp-mode))
