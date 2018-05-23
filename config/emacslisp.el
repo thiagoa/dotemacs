3a3820161cb63580625be6bf90c6ecc08a0fdade
Thiago A. Silva <thiagoaraujos@gmail.com>
2018-05-23 17:46:42 -0300

    Enable smartscan by default in Emacs Lisp

--- config/emacslisp.el
+++ config/emacslisp.el
@@ -2,5 +2,6 @@
 (add-hook 'emacs-lisp-mode-hook 'linum-mode)
 (add-hook 'emacs-lisp-mode-hook 'aggressive-indent-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'emacs-lisp-mode-hook 'smartscan-mode)
 
 (add-to-list 'auto-mode-alist '("Cask$" . emacs-lisp-mode))
