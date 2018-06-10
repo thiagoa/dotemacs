fbd5b88acd6bd7e64d1b301f6830e599194abc06
Thiago A. Silva <thiagoaraujos@gmail.com>
2018-06-10 15:54:09 -0300

    Improve structure and of looknfeel.el and use Fira code font

--- config/looknfeel.el
+++ config/looknfeel.el
@@ -1,28 +1,35 @@
 (require 'uniquify)
 
+(load-theme 'dracula t)
+(setq ansi-color-names-vector
+      ["black"
+       "PaleVioletRed3"
+       "green3"
+       "yellow3"
+       "SteelBlue1"
+       "orchid1"
+       "cyan3"
+       "gray90"])
+
+(ignore-errors (menu-bar-mode -1))
+(ignore-errors (toggle-scroll-bar -1))
+(ignore-errors (tool-bar-mode -1))
+
 (disable-annoyances)
 (disable-startup-screen)
 (show-full-filename-in-window-title)
-(load-theme 'dracula t)
-(if (functionp 'toggle-scroll-bar)(toggle-scroll-bar -1))
-(menu-bar-mode -1)
-(if (functionp 'tool-bar-mode) (tool-bar-mode -1))
 (force-split-window-sensibly-to-vertical-when-big-font)
 
 (setq mac-right-command-modifier 'meta)
-(setq my-default-font "Monaco 16")
 (setq linum-format "%d ")
+
 (setq uniquify-buffer-name-style 'post-forward)
 (setq uniquify-separator " : ")
 
+(setq my-default-font "Fira Code Retina 18")
 (add-to-list 'default-frame-alist `(font . ,my-default-font))
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(custom-set-variables '(column-number-mode t))
-
 (fontify-frame nil)
 (push 'fontify-frame after-make-frame-functions)
 
-'(custom-set-variables
-  '(ansi-color-names-vector
-    ["black" "PaleVioletRed3" "green3" "yellow3" "SteelBlue1" "orchid1" "cyan3" "gray90"]))
+(custom-set-variables '(column-number-mode t))
