c44306c091b4ba1c11738e5b65cb85058359db82
Thiago A. Silva <thiagoaraujos@gmail.com>
2018-06-10 15:53:18 -0300

    Keybindings to move the cursor faster

--- config/keybindings.el
+++ config/keybindings.el
@@ -54,6 +54,29 @@
 (global-set-key (kbd "C-c C-<")        'mc/mark-all-like-this)
 (global-set-key (kbd "C-c , r")        'tests-rerun)
 
+;;;;;;;;;;;;;;;;;;;
+;; Fast movement ;;
+;;;;;;;;;;;;;;;;;;;
+
+(global-set-key (kbd "C-S-n")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (next-line 5))))
+
+(global-set-key (kbd "C-S-p")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (previous-line 5))))
+
+(global-set-key (kbd "C-S-f")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (forward-char 5))))
+
+(global-set-key (kbd "C-S-b")
+                (lambda ()
+                  (interactive)
+                  (ignore-errors (backward-char 5))))
 
 ;;;;;;;;;;
 ;; Help ;;
