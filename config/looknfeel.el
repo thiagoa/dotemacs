3b60bf258c7a947c1d2523af38fd3323a6626dd2
Thiago A. Silva <thiagoaraujos@gmail.com>
2018-10-03 16:20:56 -0300

    Reorder stuff

--- config/looknfeel.el
+++ config/looknfeel.el
@@ -1,7 +1,25 @@
 (require 'uniquify)
 (require 'ivy-rich)
 
+(ignore-errors (menu-bar-mode -1))
+(ignore-errors (scroll-bar-mode -1))
+(ignore-errors (tooltip-mode -1))
+(ignore-errors (tool-bar-mode -1))
+
 (load-theme 'doom-one t)
+(ivy-rich-mode)
+(disable-annoyances)
+(disable-startup-screen)
+(show-full-filename-in-window-title)
+(force-split-window-sensibly-to-vertical-when-big-font)
+
+(setq mac-right-command-modifier 'meta)
+(setq linum-format "%d ")
+(setq uniquify-buffer-name-style 'post-forward)
+(setq uniquify-separator " : ")
+(setq my-default-font "Menlo 15")
+(setq ns-use-proxy-icon nil)
+(setq frame-title-format nil)
 (setq ansi-color-names-vector
       ["black"
        "PaleVioletRed3"
@@ -12,32 +30,13 @@
        "cyan3"
        "gray90"])
 
-(ivy-rich-mode)
-
-(ignore-errors (menu-bar-mode -1))
-(ignore-errors (scroll-bar-mode -1))
-(ignore-errors (tooltip-mode -1))
-(ignore-errors (tool-bar-mode -1))
-
-(disable-annoyances)
-(disable-startup-screen)
-(show-full-filename-in-window-title)
-(force-split-window-sensibly-to-vertical-when-big-font)
-
-(setq mac-right-command-modifier 'meta)
-(setq linum-format "%d ")
-
-(setq uniquify-buffer-name-style 'post-forward)
-(setq uniquify-separator " : ")
-
-(setq my-default-font "Menlo 15")
 (add-to-list 'default-frame-alist `(font . ,my-default-font))
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 (add-to-list 'default-frame-alist '(ns-appearance . dark))
-(setq ns-use-proxy-icon nil)
-(setq frame-title-format nil)
+
 (fontify-frame nil)
+
 (push 'fontify-frame after-make-frame-functions)
 
 (custom-set-variables '(column-number-mode t))
