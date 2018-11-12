192bd85e1b3f5105c0e0260aacc28990c92d6c62
Thiago A. Silva <thiagoaraujos@gmail.com>
2018-11-11 23:24:38 -0300

    Use doom-vibrant by default

--- config/looknfeel.el
+++ config/looknfeel.el
@@ -5,7 +5,7 @@
 (ignore-errors (tooltip-mode -1))
 (ignore-errors (tool-bar-mode -1))
 
-(load-theme 'doom-spacegrey t)
+(load-theme 'doom-vibrant t)
 (disable-annoyances)
 (disable-startup-screen)
 (show-full-filename-in-window-title)
