40b32ed6dac27035123c7835fded584bb65f4811
Thiago A. Silva <thiagoaraujos@gmail.com>
2019-12-04 22:22:32 -0300

    Configure graphql-mode

---
+++ config/langs/graphql.el
@@ -0,0 +1,10 @@
+;;; graphql.el --- CoffeScript config
+;;
+;;; Commentary:
+
+;;; Code:
+
+(add-hook 'graphql-mode-hook (lambda ()
+                               (safe-linum-mode)))
+
+;;; graphql.el ends here
