ae6a9d270959399d3db55d4d0c5cbd09602e5db2
Thiago A. Silva <thiagoaraujos@gmail.com>
2025-08-20 15:53:42 -0300

    Improve Elixir project recognition

--- config/langs/elixir.el
+++ config/langs/elixir.el
@@ -15,5 +15,11 @@
 (add-hook 'elixir-mode-hook 'smartscan-mode)
 (add-hook 'elixir-mode-hook (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
 (add-to-list 'elixir-mode-hook 'auto-activate-ruby-end-mode-for-elixir-mode)
+;; Place Elixir projects first to avoid them being confused with Yarn
+;; projects. Elixir projects often have Yarn artifacts.
+(let* ((found (cl-find-if (lambda (v) (eq (car v) 'elixir)) projectile-project-types)))
+  (when found
+    (setq projectile-project-types
+          (cons found (remove found projectile-project-types)))))
 
 ;;; elixir.el ends here
