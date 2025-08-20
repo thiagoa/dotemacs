7ee1aedbe83504fda7d946e052dc86845b773346
Thiago A. Silva <thiagoaraujos@gmail.com>
2025-08-20 15:51:58 -0300

    Function to export a file with local env variables

--- config/environment.el
+++ config/environment.el
@@ -4,11 +4,27 @@
 
 ;;; Code:
 
+(require 'projectile)
+
 (setenv "LESS_OPEN" nil)
 (setenv "VISUAL" "emacsclient")
 (setenv "EDITOR" (getenv "VISUAL"))
 (setenv "PAGER" "cat")
 (setenv "TERM" "xterm-256-color")
 (setenv "SKIP_COVERAGE" "1")
+
+(defun load-env-file (file)
+  "Load environment variables from FILE into Emacs' environment."
+  (setq file (concat (projectile-project-root) ".env.development"))
+  (when (file-exists-p file)
+    (with-temp-buffer
+      (insert-file-contents file)
+      (dolist (line (split-string (buffer-string) "\n" t))
+        (unless (or (string-prefix-p "#" line) ; skip comments
+                    (string-blank-p line))    ; skip blanks
+          (when (string-match "\\`\\([^=]+\\)=\\(.*\\)\\'" line)
+            (let ((key (match-string 1 line))
+                  (value (match-string 2 line)))
+              (setenv key value))))))))
 
 ;;; environment.el ends here
